8e34855acf62919a078fa56822545edf9c29454a
Matthew <eossu@kakao.com>
2022-03-07 16:47:31 +0900

    Feat: 추가 - 회원가입뷰 TextField 바깥 터치 시 키보드 dismiss 시킴/ 로그인 실패시 alert 코멘트 변경

--- MovieRanking/ViewController/RegisterViewController.swift
+++ MovieRanking/ViewController/RegisterViewController.swift
@@ -27,6 +27,8 @@ class RegisterViewController: UIViewController {
                 self?.alertService()
             }
         }
+        
+        self.hideKeyboardWhenTappedAround()
     }
     
     @IBAction func registerButton(_ sender: Any) {
